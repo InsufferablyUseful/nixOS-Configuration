795d127982a68aa5be776d9151d2666912e38f84
InsufferablyUseful <benrobertson150@hotmail.co.uk>
2023-10-05 22:44:19 +0100

    Add initial desktop setup

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     # which represents the GitHub repository URL + branch/commit-id/tag.
 
     # Official NixOS package source, using nixos-unstable branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     #Home-manager manages user packages and dotfiles declaratively
     home-manager = {
       url = "github:nix-community/home-manager/release-23.05";
@@ -16,23 +16,69 @@
       # to avoid problems caused by different versions of nixpkgs.
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    
+    hyprland.url = "github:hyprwm/Hyprland";
   };
 
- outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-    nixosConfigurations = {
-        "nixosPlayground" = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-       ./hosts/nixos-playground 
-       home-manager.nixosModules.home-manager
-       {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.users.ben = import ./home;
-       }
+ outputs = { self, nixpkgs, home-manager, ... }@inputs:
+    let
+      inherit (self) outputs;
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
       ];
-   };
-};
-};
+    in
+    rec {
+      # Your custom packages
+      # Acessible through 'nix build', 'nix shell', etc
+      #packages = forAllSystems (system:
+        #let pkgs = nixpkgs.legacyPackages.${system};
+        #in import ./pkgs { inherit pkgs; }
+      #);
+      # Devshell for bootstrapping
+      # Acessible through 'nix develop' or 'nix-shell' (legacy)
+      #devShells = forAllSystems (system:
+        #let pkgs = nixpkgs.legacyPackages.${system};
+        #in import ./shell.nix { inherit pkgs; }
+      #);
 
+      # Your custom packages and modifications, exported as overlays
+      #overlays = import ./overlays { inherit inputs; };
+      # Reusable nixos modules you might want to export
+      # These are usually stuff you would upstream into nixpkgs
+      #nixosModules = import ./modules/nixos;
+      # Reusable home-manager modules you might want to export
+      # These are usually stuff you would upstream into home-manager
+      #homeManagerModules = import ./modules/home-manager;
+
+      # NixOS configuration entrypoint
+      # Available through 'nixos-rebuild --flake .#your-hostname'
+      nixosConfigurations = {
+        # FIXME replace with your hostname
+        home-desktop = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [
+            # > Our main nixos configuration file <
+            ./hosts/home-desktop
+          ];
+        };
+      };
+
+      # Standalone home-manager configuration entrypoint
+      # Available through 'home-manager --flake .#your-username@your-hostname'
+      homeConfigurations = {
+        # FIXME replace with your username@hostname
+        "ben@home-desktop" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+          extraSpecialArgs = { inherit inputs outputs; };
+          modules = [
+            # > Our main home-manager configuration file <
+            ./home
+          ];
+        };
+      };
+    };
 }
